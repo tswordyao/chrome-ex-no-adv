22a0ec52cd05167fbaaaab29bf096adc9c29f06b
evans <1280898239@qq.com>
2018-09-28 16:39:55 +0800

    up master trigger

--- ct/index.js
+++ ct/index.js
@@ -1,9 +1,10 @@
-console.info('\n\n-------no-adv-iframe running......\n\n');
+console.log('%c-------no-adv-iframe running......','padding:20px;background:aqua;');
 
 var slice=Function.prototype.call.bind([].slice);
-document.addEventListener('DOMContentLoaded',e=>{
+//document.addEventListener('DOMContentLoaded',e=>{
     console.log('\n-----DOMContentLoaded\n');
-    setTimeout(function(){
+    //setTimeout(function(){
+        var style='';
         try{
             [   '.wgt-daily',
                 'ec-ad',
@@ -24,24 +25,28 @@ document.addEventListener('DOMContentLoaded',e=>{
                 '.recommend-right',
                 'iframe'
             ].forEach(s=>{
-                document.querySelectorAll(s).forEach( ele => ele.setAttribute('style',(ele.getAttribute('style')||'')+';visibility:hidden!important;') );
+                style+=`${s}{visibility:hidden!important;}`;
+                //document.querySelectorAll(s).forEach( ele => ele.setAttribute('style',(ele.getAttribute('style')||'')+';visibility:hidden!important;') );
             });
+            var styleEle = document.createElement('style');
+            styleEle.innerText=style;
+            document.documentElement.appendChild(styleEle);
         }catch(e){
             console.error(e);
         }
-    },200);
+    //},0);
 
+//});
 
-    chrome.extension.onMessage.addListener(
-        function(request, sender, sendResponse) {
-            document.querySelectorAll('img').forEach(ele => {
-                ele.remove();
-            });
-            alert("前端/后端/Popup收到");
-            sendResponse("popup返回值");
-        }
-    );
-});
+chrome.extension.onMessage.addListener(
+    function(request, sender, sendResponse) {
+        document.querySelectorAll('img').forEach(ele => {
+            ele.remove();
+        });
+        alert("前端/后端/Popup收到");
+        sendResponse("popup返回值");
+    }
+);
 
 1||chrome.extension.onRequest.addListener( (request, sender, sendResponse) => {
       console.log( sender.tab ?
